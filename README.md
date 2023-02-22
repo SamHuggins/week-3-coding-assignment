f997a4c0ca6ff6ff82fce3f7c498d4390653a375
SamHuggins <123792876+SamHuggins@users.noreply.github.com>
2023-02-22 23:04:39 +0000

    finished question 1, started question 2

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-ECHO is on.
+1.  Create an array called ages that contains the following values: 3, 9, 23, 64, 2, 8, 28, 93.
+
+1a.Programmatically subtract the value of the first element in the array from the value in the last element of the array.   
+Do not use numbers to reference the last element, find it programmatically.
+ages[7] – ages[0] is not allowed!
+1b. Add a new age to your array and repeat the step above to ensure it is dynamic. (works for arrays of different lengths).
+1c. Use a loop to iterate through the array and calculate the average age. 
+
+2.  Create an array called names that contains the following values: ‘Sam’, ‘Tommy’, ‘Tim’, ‘Sally’, ‘Buck’, ‘Bob’.
+2a. Use a loop to iterate through the array and calculate the average number of letters per name. 
+2b. Use a loop to iterate through the array again and concatenate all the names together, separated by spaces. 
--- index.js
+++ index.js
@@ -1,24 +1,39 @@
-
 var ages = [3, 9, 23, 64, 2, 8, 28, 93];
+console.log("ages array: ", ages);
 
-//this doesn't work because the vars would have to be redeclared every time a new age was pushed
-//could turn this into a function
-//var firstAge = ages[ages.length - ages.length];
-//var lastAge = ages[ages.length - 1];
-//console.log(lastAge - firstAge);
-
+console.log("Ages last - Ages first");
 console.log(ages[ages.length - 1] - ages[ages.length - ages.length]);
 
-ages.push(7);
+ages.push(13);
+console.log("New ages array: ", ages);
+console.log("Ages last - Ages first");
 console.log(ages[ages.length - 1] - ages[ages.length - ages.length]);
 
-//this section doesnt work yet
+
 var num = 0
 for(let i = 0; i < ages.length; i++){
     num = num + ages[i]
     if (i === (ages.length - 1)) {
-        console.log(ages.length);
-        console.log((num / (ages.length)));
+      console.log("Ages added together: ",num);  
+      console.log("number of elements in the array: ", ages.length);
+      console.log("Array avg: ");
+      console.log((num / (ages.length)));
     }
 
 }
+
+console.log("question 2")
+var names = ["Sam", "Tommy", "Tim", "Sally", "Buck", "Bob"];
+
+num = 0
+for(let i = 0; i <= (names.length - 1) ; i++){
+  num = num + names[i].length
+  if(i === (names.length - 1)) {
+    console.log("Legnth of str: ", num);
+    console.log("Number of elements in an array= ", names.length)
+    console.log("Average length of name: ", num / names.length);
+  }
+}
+
+console.log("Names concatinated: ");
+
